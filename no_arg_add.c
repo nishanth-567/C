185a96bdab34babe1ee49fddeeb798d1ece8604c
Nishanth Mandava <mandavanishanth@gmail.com>
2025-08-07 05:49:48 +0100

    Add files via upload

---
+++ no_arg_add.c
@@ -0,0 +1,16 @@
+
+// Nishanth, Roll no: 24217, CSE-C
+// Function with no argument and no return type
+
+#include <stdio.h>
+void addition();
+void main()
+{
+    addition();
+}
+void addition()
+{
+    int sum, a = 10, b = 45;
+    sum = a + b;
+    printf("%d + %d  = %d", a,b,sum);
+}
